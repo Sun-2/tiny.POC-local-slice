52d158af891f733e460c55ebf905c41e9bdc30a2
Sun-2 <sun127.0.0.1@gmail.com>
2020-10-28 18:59:52 +0100

    createlocalslice

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,105 @@
 import React, {
-  createContext,
-  Dispatch, useContext,
+  Dispatch,
+  memo,
+  useContext,
   useEffect,
   useMemo,
   useReducer,
   useState,
 } from "react";
 import { AnyAction, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createContext, useContextSelector } from "use-context-selector";
 
-const initialState = {
-  counter: 0,
+const reducers = {
+  increase: (state: any, action: PayloadAction) => {
+    state.counter += 1;
+  },
 };
-const slice = createSlice({
-  name: "main",
-  initialState,
-  reducers: {
-    increase: (state, action: PayloadAction) => {
+
+const createLocalSlice = <T extends any>(initialState: T, reducers: any) => {
+  const { reducer, actions } = createSlice({
+    name: "local-slice",
+    initialState,
+    reducers,
+  });
+
+  const Context = createContext(
+    (null as unknown) as { state: T; dispatch: Dispatch<any> }
+  );
+
+  const useDispatch = () =>
+    useContextSelector(Context, (state) => state.dispatch);
+
+  const useSelector = (selector: (state: T) => unknown) =>
+    useContextSelector(Context, (state) => selector(state.state));
+
+  const useSlice = (innerInitialState?: T) => {
+    const [state, dispatch] = useReducer(
+      reducer,
+      innerInitialState || initialState
+    );
+    const Provider = useMemo(
+      () => (props: any) => (
+        <Context.Provider value={{ state, dispatch }} {...props} />
+      ),
+      [state]
+    );
+
+    return {
+      Provider,
+      state,
+      dispatch,
+    };
+  };
+
+  return {
+    actions,
+    useDispatch,
+    useSelector,
+    useSlice,
+  };
+};
+
+const { actions, useDispatch, useSelector, useSlice } = createLocalSlice(
+  { counter: 0, reduntant: 0 },
+  {
+    increase: (state, action) => {
       state.counter += 1;
     },
-  },
-});
+  }
+);
 
-
-const Ctx = createContext(null as any);
-
-const Compo = (props: { dispatch: Dispatch<AnyAction> }) => {
-  const dispatch = useContext(Ctx).dispatch;
+const Compo = (props) => {
+  const dispatch = useDispatch();
   const onClick = () => {
-    dispatch(slice.actions.increase());
+    // @ts-ignore
+    dispatch(actions.increase());
   };
   return <button onClick={onClick}>Increase</button>;
 };
 
+const Value = () => {
+  const value = useSelector((state) => state.counter);
+  return <p>Value is: {value}</p>;
+};
 
+const Reduntant = memo(() => {
+  const value = useSelector((state) => state.reduntant);
+  console.log("REDUNTANT RENDER");
+
+  return <p>Hi, I'm here. Rerenders: {value}</p>;
+});
 
 function App() {
-  const [token, setToken] = useState("");
-  const [state, dispatch] = useReducer(slice.reducer, initialState);
-  const ctxValue = useMemo(
-    () => ({
-      state,
-      dispatch,
-    }),
-    [state]
-  );
-
-  /*useEffect(() => {
-    (async () => {
-      grecaptcha.ready(async () => {
-        const token = await grecaptcha.execute(
-          "6LfRbtwZAAAAAN1ai_N03a4bGB6NN2iK5ZeGciHE",
-          {
-            action: "submit",
-          }
-        );
-        setToken("loading...");
-        const resp = await fetch("http://localhost:3001/recaptcha-validate", {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({
-            token,
-          }),
-        });
-        const json = await resp.json();
-        setToken(JSON.stringify(json));
-      });
-    })();
-  }, []);*/
+  const { Provider, state } = useSlice();
   return (
     <>
-      <Ctx.Provider value={ctxValue}>
+      <Provider>
         <p>Hello! Counter is {state.counter}</p>
-        <Compo dispatch={dispatch} />
-      </Ctx.Provider>
+        <Compo />
+        <Value />
+        <Reduntant />
+      </Provider>
     </>
   );
 }
